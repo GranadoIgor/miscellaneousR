94c2c986983a58869ab291706b24b42aa14c82d1
GranadoIgor <igranado@azti.es>
2023-09-20 12:54:19 +0200

    add welcome message

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+
+.onAttach <- function(libname,pkgname){
+  packageStartupMessage(paste0("Welcome to miscellaneousR. Using version: ", packageVersion("miscellaneousR"), ".",
+                        "\nMore info: https://github.com/GranadoIgor/miscellaneousR"))
+}
+
+
+
